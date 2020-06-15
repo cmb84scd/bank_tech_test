e6cf28eae71c4ccad4aa69ce447f923c29126d53
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-15 16:34:30 +0100

    Add withdraw money functionality

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,4 +13,10 @@ describe Account do
     @account.deposit(500)
     expect(@account.show_balance).to eq 500
   end
+
+  it 'Allows you to withdraw money' do
+    @account.deposit(500)
+    @account.withdraw(100)
+    expect(@account.show_balance).to eq 400
+  end
 end
