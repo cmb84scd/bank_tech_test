63f20006cc08ed76e6c32f517524c10be17eb9d2
Catriona <catrionamairi@blueyonder.co.uk>
2020-06-17 11:02:24 +0100

    Refactor tests to use let instead of before each

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,26 +1,24 @@
 require 'account'
 
 describe Account do
-  before(:each) do
-    @account = Account.new
-  end
-
+  let(:account) { account = Account.new }
+  
   it 'has a default balance of 0' do
-    expect(@account.show_balance).to eq 0
+    expect(account.show_balance).to eq 0
   end
 
   it 'allows you to save money' do
-    expect { @account.deposit(500) }.to change { @account.show_balance }.by 500
+    expect { account.deposit(500) }.to change { account.show_balance }.by 500
   end
 
   describe '#withdraw' do
     it 'allows you to withdraw money' do
-      @account.deposit(500)
-      expect { @account.withdraw(100) }.to change { @account.show_balance }.by(-100)
+      account.deposit(500)
+      expect { account.withdraw(100) }.to change { account.show_balance }.by(-100)
     end
 
     it 'raises an error if not enough money' do
-      expect { @account.withdraw(100) }.to raise_error("Not enough money in your account! Your balance is: #{@account.show_balance}")
+      expect { account.withdraw(100) }.to raise_error("Not enough money in your account! Your balance is: #{account.show_balance}")
     end
   end
 
@@ -29,35 +27,35 @@ describe Account do
 
   describe '#transactions' do
     it 'has an empty transactions list by default' do
-      expect(@account.transactions).to be_empty
+      expect(account.transactions).to be_empty
     end
 
     it 'can store a single transaction' do
-      @account.deposit(500)
-      expect(@account.transactions).to include save
+      account.deposit(500)
+      expect(account.transactions).to include save
     end
 
     it 'can store multiple transactions' do
-      @account.deposit(500)
-      @account.withdraw(100)
-      expect(@account.transactions).to include spend
+      account.deposit(500)
+      account.withdraw(100)
+      expect(account.transactions).to include spend
     end
   end
 
   it 'can a print a statement header' do
-    expect(@account.statement_header).to eq "date || credit || debit || balance"
+    expect(account.statement_header).to eq "date || credit || debit || balance"
   end
 
   describe '#statement' do
     it 'can print a simple statement' do
-      @account.deposit(500)
-      expect(@account.statement).to eq [save]
+      account.deposit(500)
+      expect(account.statement).to eq [save]
     end
 
     it 'can print a statement' do
-      @account.deposit(500)
-      @account.withdraw(100)
-      expect(@account.statement).to eq [save, spend]
+      account.deposit(500)
+      account.withdraw(100)
+      expect(account.statement).to eq [save, spend]
     end
   end
 end
